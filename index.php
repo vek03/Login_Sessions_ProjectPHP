aa17dd6ed80d5720b94033227b655aec22f156c2
vek03 <vektromboni@gmail.com>
2022-06-02 16:08:15 -0300

    Arquivos do Site

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sessão PHP</title>
+</head>
+<body>
+    <form method="post" action="ope.php" id="formlogin" name="formlogin">
+        <label>Nome : </label>
+        <input type="text" name="login" id="login"><br><br>
+        <label>Senha : </label>
+        <input type="password" name="senha" id="senha"><br><br>
+        <input type="submit" value="LOGAR">
+    </form>    
+</body>
+</html>
---
+++ ope.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+$_SESSION['login'] = $_POST['login'];
+$_SESSION['senha'] = $_POST['senha'];
+
+header('location:site.php');
+
+?>
